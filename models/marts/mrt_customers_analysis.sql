573d727270c03625f626ac32b92664863a0162bd
TV CA <tnguyen13@outlook.fr>
2024-11-23 15:51:58 +0000

    update _stg_schema.yml

--- models/marts/mrt_customers_analysis.sql
+++ models/marts/mrt_customers_analysis.sql
@@ -2,6 +2,45 @@
     materialized='table'
 ) }}
 
+WITH query_sales_by_customer AS (
+
+SELECT 
+customer_id,
+order_at,
+SUM(order_items_total_price) as order_price_by_customer
+
+FROM 
+
+{{ref('int_orders_items_sales')}}
+
+GROUP BY
+
+customer_id,
+order_at
+),
+
+query_customer_month_diff_order AS (
+
+SELECT 
+customer_id,
+DATE_DIFF(MAX(order_at),MAX(last_order_at), MONTH) AS last_order_at_diff_month
+
+FROM (
+  SELECT
+
+  customer_id,
+  order_at,
+  LAG(order_at) OVER(ORDER BY order_at) as last_order_at
+
+  FROM 
+
+  query_sales_by_customer 
+)
+
+group by customer_id
+
+)
+
 SELECT
 
 oi.customer_id,
@@ -18,7 +57,8 @@ quantity,
 product_price, 
 order_at,
 order_date_year_month,
-order_items_total_price
+order_items_total_price,
+last_order_at_diff_month
 
 FROM 
 
@@ -27,3 +67,6 @@ LEFT JOIN {{ref("int_products_details")}} p
 ON oi.product_id = p.product_id
 LEFT JOIN {{ref("stg_customers")}} c 
 ON oi.customer_id = c.customer_id
+LEFT JOIN query_customer_month_diff_order d 
+ON oi.customer_id = d.customer_id
+
